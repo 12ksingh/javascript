d5c46a08bb6aee530ddae96971cd1b3a06b741cd
kuldeep singh <108728408+12ksingh@users.noreply.github.com>
2024-05-30 13:05:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let name = 'kuldeep';
+console.log(name);
+console.log('jeep');
+//cannot be reseved 
+//shoiuld be meaingfull 
+//cannot start with number
+
+let firstName = 'chetak'; let lastName = 'rana';
+console.log(firstName); console.log(lastName);
